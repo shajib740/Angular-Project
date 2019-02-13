fb2438abace640d08ef18f1e75b3819402367a8b
shajibchy <shajib740@gmail.com>
2019-02-14 01:06:53 +0600

    Text File Added

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses For Test</h2>'
+    template: `
+        <h2>{{ "Title :"+ getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course}}
+            </li>
+        </ul>
+        `
 })
 export class CoursesComponent {
+    title = "List of courses";
+    courses;
 
+    constructor(service: CoursesService){
+        // let service = new CoursesService
+        this.courses = service.getCourses ();
+    }
+    getTitle(){
+        return this.title;
+    }
+  
+    //Logic for calling HTTP service
 }
 
-function Console(target) {
-	console.log(target);
-}
-
-@Console
-class ExampleClass {
-	constructor() {
-  	console.log('Yo!');
-  }
-}
 
